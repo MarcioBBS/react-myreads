3d6bb91e539a8f66dcac31461ded83fa0dde4864
MacioBBS <marciobbs@gmail.com>
2021-04-12 21:05:11 -0700

    Create method renderShelf

--- src/Shelf.js
+++ src/Shelf.js
@@ -4,9 +4,25 @@ import Book from "./Book";
 import "./App.css";
 
 class Shelf extends Component {
+  renderShelf(books, shelfTitle, shelfID) {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books
+              .filter(b => b.shelf === shelfID)
+              .map(b => (
+                <Book key={b.id} book={b} />
+              ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+
   render() {
     const { books } = this.props;
-    console.log(books);
 
     return (
       <div className="list-books">
@@ -15,44 +31,14 @@ class Shelf extends Component {
         </div>
 
         <div className="list-books-content">
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Currently Reading</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {books
-                  .filter(b => b.shelf === "currentlyReading")
-                  .map(b => (
-                    <Book key={b.id} book={b} />
-                  ))}
-              </ol>
-            </div>
-          </div>
+          {/* Render Shelf Currently Reading */}
+          {this.renderShelf(books, "Currently Reading", "currentlyReading")}
 
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Want to Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {books
-                  .filter(b => b.shelf === "wantToRead")
-                  .map(b => (
-                    <Book key={b.id} book={b} />
-                  ))}
-              </ol>
-            </div>
-          </div>
+          {/* Render Shelf Wanto to Read */}
+          {this.renderShelf(books, "Want to Read", "wantToRead")}
 
-          <div className="bookshelf">
-            <h2 className="bookshelf-title">Read</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {books
-                  .filter(b => b.shelf === "read")
-                  .map(b => (
-                    <Book key={b.id} book={b} />
-                  ))}
-              </ol>
-            </div>
-          </div>
+          {/* Render Shelf Wanto to Read */}
+          {this.renderShelf(books, "Read", "read")}
         </div>
 
         <div className="open-search">

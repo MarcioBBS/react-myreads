0eb179cbdfae2f1d169a1e89ae39958fcf90d275
MacioBBS <marciobbs@gmail.com>
2021-04-15 10:39:28 -0700

    Refactor - To functional component

--- src/Shelf.js
+++ src/Shelf.js
@@ -4,41 +4,39 @@ import PropTypes from "prop-types";
 import Book from "./Book";
 import "./App.css";
 
-class Shelf extends Component {
-  render() {
-    const { books, updateShelf } = this.props;
-    const shelves = [{ name: "Currently Reading", id: "currentlyReading" }, { name: "Want to Read", id: "wantToRead" }, { name: "Read", id: "read" }];
+const Shelf = props => {
+  const { books, updateShelf } = props;
+  const shelves = [{ name: "Currently Reading", id: "currentlyReading" }, { name: "Want to Read", id: "wantToRead" }, { name: "Read", id: "read" }];
 
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-
-        <div className="list-books-content">
-          {shelves.map(shelf => (
-            <div key={shelf.id} className="bookshelf">
-              <h2 className="bookshelf-title">{shelf.name}</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {books
-                    .filter(b => b.shelf === shelf.id)
-                    .map(b => (
-                      <Book key={b.id} book={b} updateShelf={updateShelf} />
-                    ))}
-                </ol>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="open-search">
-          <Link to="/searchbooks">Add a book</Link>
-        </div>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    );
-  }
-}
+
+      <div className="list-books-content">
+        {shelves.map(shelf => (
+          <div key={shelf.id} className="bookshelf">
+            <h2 className="bookshelf-title">{shelf.name}</h2>
+            <div className="bookshelf-books">
+              <ol className="books-grid">
+                {books
+                  .filter(b => b.shelf === shelf.id)
+                  .map(b => (
+                    <Book key={b.id} book={b} updateShelf={updateShelf} />
+                  ))}
+              </ol>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="open-search">
+        <Link to="/searchbooks">Add a book</Link>
+      </div>
+    </div>
+  );
+};
 
 Shelf.propTypes = {
   books: PropTypes.array.isRequired,
